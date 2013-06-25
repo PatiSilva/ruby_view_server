470602537be06718c503d48ceec7c2e11d93086e
Jason Watson <jswatson0@gmail.com>
2013-06-25 15:18:56 -0500

    added jason.html.erb view

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,9 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+x = "This is a string"
+puts template.result(binding)
+template = ERB.new "The value of x is: <%= x %>"
+x = {key: 'value'}
+template = ERB.new "The value of x is: <%= x %>"
+puts template.result(binding)
